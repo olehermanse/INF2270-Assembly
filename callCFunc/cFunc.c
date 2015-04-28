c22743a42a288fc67b791955b7c09a73f744abe1
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2015-04-28 04:25:42 +0200

    Cross Platform Support

--- callCFunc/cFunc.c
+++ callCFunc/cFunc.c
@@ -2,3 +2,8 @@
 int cFunc(int input){
 	return input * 2;	// Must do something, to ensure function call worked
 }
+
+// For Cross platform compatibility Linux <-> Mac OSX :
+int _cFunc(int input){
+	return cFunc(input);
+}
